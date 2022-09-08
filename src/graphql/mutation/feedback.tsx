80e592481d0fe30e867d00cadd100e2a93dcc3e0
grpse2021 <86371914+grpse2021@users.noreply.github.com>
2022-09-08 18:40:08 +0530

    Update feedback.tsx

--- src/graphql/mutation/feedback.tsx
+++ src/graphql/mutation/feedback.tsx
@@ -72,8 +72,8 @@ export const POST_PATIENT_FEEDBACK = gql`
         answer
         created_date
         patient_id
-        pttherapy_id
         question_id
+        pttherapy_id
         status
         therapist_id
         updated_date
