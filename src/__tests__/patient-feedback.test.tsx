14ce3ae601fd9694dea28c22d19bd8e5cfd39c26
PrasannaMyhelp <prasanna.nehru@gumi.sg>
2022-09-09 00:02:48 +0800

    test cases fixes

--- src/__tests__/patient-feedback.test.tsx
+++ src/__tests__/patient-feedback.test.tsx
@@ -37,7 +37,7 @@ const getPatientTherapy = (
     patient_id: _patient_id,
     patientsTherapy: [
       {
-        _id: Guid.create().toString(),
+        _id: "test-1",
         patient_id: _patient_id,
         therapy_detail: {
           therapy_name: "localhost",
@@ -244,6 +244,19 @@ const buildMocks = (): {
     _mocks.push({
       request: {
         query: POST_PATIENT_FEEDBACK,
+        variables: {
+          feedQuesAnsData:[
+            {
+              therapist_id: "686802e5123a482681a680a673ef7f53",
+              answer: "s",
+              session_no:1, 
+              question_id: "9b04def7-c012-44ca-98f2-6060d90b9a25"
+            }
+          ],
+          sessionNo: 1,
+          feedbackType: "session",
+          pttherapyId: "test-1"
+        }
       },
       result: {
         data: {
@@ -258,7 +271,7 @@ const buildMocks = (): {
               created_date: "2022-07-09T15:53:28.900Z",
               patient_id: "4937a27dc00d48bf983fdcd4b0762ebd",
               question_id: "9b04def7-c012-44ca-98f2-6060d90b9a25",
-              pttherapy_id: "1116ce468e9b4977b8943f2d8e9f2b0b",
+              pttherapy_id: "test-1",
               status: "active",
               therapist_id: "686802e5123a482681a680a673ef7f53",
               updated_date: "2022-07-09T15:53:28.900Z",
@@ -286,7 +299,7 @@ const buildMocks = (): {
           variables: {
             sessionNo: 1,
             feedbackType: "session",
-            pttherapyId: _pt._id,
+            pttherapyId: "test-1",
           },
         },
         result: {
@@ -472,11 +485,9 @@ describe("Patient feedback list", () => {
       const p = _k + 1;
       const panelName = "panel" + p;
       fireEvent.click(screen.queryByTestId(panelName + "bh-header"));
-      fireEvent.click(screen.getByLabelText("p"));
+      fireEvent.click(screen.getByLabelText("s"));
       fireEvent.submit(screen.queryByTestId("feedbackForm"));
-      expect(
-        screen.getByText("Feedback submitted successfully")
-      ).toBeInTheDocument();
+      expect(screen.queryByTestId("submitFeedback")).toBeDisabled();
     });
   });
 });
