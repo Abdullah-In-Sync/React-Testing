1f24803e967a47bf0cead3eb3f6c9ae639c0e8de
Ravindra Bhanderi <ravindra.bhanderi@myhelp.co.uk>
2022-11-15 20:48:58 +0530

    Feat:  add authorization in add resource page, test case update

--- src/hoc/auth.tsx
+++ src/hoc/auth.tsx
@@ -1,16 +1,14 @@
-import { useRouter } from "next/router";
 import { useEffect } from "react";
 import { useAppContext } from "../contexts/AuthContext";
+import { env } from "../lib/env";
 
 export default function withAuthentication<T>(
   Component: React.ComponentType<T>
 ) {
   return (props: T) => {
     const { isAuthenticated } = useAppContext();
-    const router = useRouter();
-
     useEffect(() => {
-      if (!isAuthenticated) router.push("/account");
+      if (!isAuthenticated) window.location.href = env.v1.rootUrl;
     });
 
     return <>{isAuthenticated ? <Component {...props} /> : null}</>;
