3975111b1e810678b827f99ec162e62271c2ae02
ravindrabhanderi14 <117576691+ravindrabhanderi14@users.noreply.github.com>
2022-11-16 17:34:22 +0530

    Feat: therapist list  resource page, test case update

--- src/__tests__/therapist-resource.test.tsx
+++ src/__tests__/therapist-resource.test.tsx
@@ -209,14 +209,18 @@ describe("Therapist Resource page", () => {
   beforeEach(() => {
     (useAppContext as jest.Mock).mockReturnValue({
       isAuthenticated: true,
-      user: {
+      user:{
         _id: "9ea296b4-4a19-49b6-9699-c1e2bd6fc946",
-        user_type: "admin",
+        user_type: "therapist",
         parent_id: "73ddc746-b473-428c-a719-9f6d39bdef81",
         perm_ids: "9,10,14,21,191,65,66",
         user_status: "1",
         created_date: "2021-12-20 16:20:55",
         updated_date: "2021-12-20 16:20:55",
+        therapist_data: {
+          _id: "therapist_id",
+          org_id: "myhelp",
+        },
       },
     });
   });
