7b53a305cc04c82ac93404a8578f41d2cee9e4cb
ravindrabhanderi14 <117576691+ravindrabhanderi14@users.noreply.github.com>
2022-11-16 17:29:26 +0530

    Feat:  add authorization in detail therapist detail resource page, test case update

--- src/__tests__/therapist-add-resource.test.tsx
+++ src/__tests__/therapist-add-resource.test.tsx
@@ -20,10 +20,14 @@ import { CREATE_RESOURCE } from "../graphql/mutation/resource";
 import * as s3 from "../lib/helpers/s3";
 
 import { useRouter } from "next/router";
+import { useAppContext } from "../contexts/AuthContext";
 jest.mock("next/router", () => ({
   __esModule: true,
   useRouter: jest.fn(),
 }));
+
+jest.mock("../contexts/AuthContext");
+
 // mocks
 const mocksData = [];
 // disorder
@@ -191,6 +195,21 @@ const sut = async () => {
 };
 
 describe("Admin add resource page", () => {
+  beforeEach(() => {
+    (useAppContext as jest.Mock).mockReturnValue({
+      isAuthenticated: true,
+      user: {
+        _id: "9ea296b4-4a19-49b6-9699-c1e2bd6fc946",
+        user_type: "admin",
+        parent_id: "73ddc746-b473-428c-a719-9f6d39bdef81",
+        perm_ids: "9,10,14,21,191,65,66",
+        user_status: "1",
+        created_date: "2021-12-20 16:20:55",
+        updated_date: "2021-12-20 16:20:55",
+      },
+    });
+  });
+
   it("should render complete add resource form", async () => {
     await sut();
     expect(screen.getByTestId("resource-add-form")).toBeInTheDocument();
