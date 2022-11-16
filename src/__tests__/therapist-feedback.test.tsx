17586d72e3832f5d1196686803e3ae47010f9032
ravindrabhanderi14 <117576691+ravindrabhanderi14@users.noreply.github.com>
2022-11-16 18:01:42 +0530

    Feat: therapist feedback page, test case update

--- src/__tests__/therapist-feedback.test.tsx
+++ src/__tests__/therapist-feedback.test.tsx
@@ -13,6 +13,9 @@ import {
 import { GET_PATIENTSESSION_DATA } from "../graphql/query/patient";
 import { GET_THERAPISTFEEDBACKLIST_DATA } from "../graphql/query";
 import { Guid } from "guid-typescript";
+import { useAppContext } from "../contexts/AuthContext";
+
+jest.mock("../contexts/AuthContext");
 
 interface MockOptions {
   getTherapistFeedbackList: boolean;
@@ -288,6 +291,24 @@ const { mocks, mockDataMap } = buildMocks();
 
 // tests
 describe("Therapist feedback list", () => {
+  beforeEach(() => {
+    (useAppContext as jest.Mock).mockReturnValue({
+      isAuthenticated: true,
+      user: {
+        _id: "9ea296b4-4a19-49b6-9699-c1e2bd6fc946",
+        user_type: "therapist",
+        parent_id: "73ddc746-b473-428c-a719-9f6d39bdef81",
+        perm_ids: "9,10,14,21,191,65,66",
+        user_status: "1",
+        created_date: "2021-12-20 16:20:55",
+        updated_date: "2021-12-20 16:20:55",
+        therapist_data: {
+          _id: "therapist_id",
+          org_id: "myhelp",
+        },
+      },
+    });
+  });
   test("is collaped by default", async () => {
     const patient_id = mockDataMap["first_patient_id"];
     await sut(patient_id);
