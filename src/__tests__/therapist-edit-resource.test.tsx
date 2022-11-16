a4cad4bb18df0e1a289f37f99b33b3c36336cbe6
ravindrabhanderi14 <117576691+ravindrabhanderi14@users.noreply.github.com>
2022-11-16 17:06:14 +0530

    Feat:  edit resource page test case update

--- src/__tests__/therapist-edit-resource.test.tsx
+++ src/__tests__/therapist-edit-resource.test.tsx
@@ -23,12 +23,15 @@ import { UPDATE_RESOURCE_BY_ID } from "../graphql/mutation/resource";
 import * as s3 from "../lib/helpers/s3";
 
 import { useRouter } from "next/router";
+import { useAppContext } from "../contexts/AuthContext";
 
 const pushMock = jest.fn();
 
 jest.mock("next/router", () => ({
   useRouter: jest.fn(),
 }));
+
+jest.mock("../contexts/AuthContext");
 
 const mocksData = [];
 
@@ -331,6 +334,18 @@ const sut = async () => {
 describe("Admin edit resource page", () => {
   beforeEach(() => {
     (useRouter as jest.Mock).mockClear();
+    (useAppContext as jest.Mock).mockReturnValue({
+      isAuthenticated: true,
+      user: {
+        _id: "9ea296b4-4a19-49b6-9699-c1e2bd6fc946",
+        user_type: "admin",
+        parent_id: "73ddc746-b473-428c-a719-9f6d39bdef81",
+        perm_ids: "9,10,14,21,191,65,66",
+        user_status: "1",
+        created_date: "2021-12-20 16:20:55",
+        updated_date: "2021-12-20 16:20:55",
+      },
+    });
   });
 
   it("should render complete edit resource form input field", async () => {
