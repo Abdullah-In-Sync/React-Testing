08c9e8c62ff6a0447b168f88e985f023812c2114
ravindrabhanderi14 <117576691+ravindrabhanderi14@users.noreply.github.com>
2022-11-17 15:22:21 +0530

    Feat: remove the old function

--- src/lib/helpers/auth.ts
+++ src/lib/helpers/auth.ts
@@ -52,28 +52,6 @@ export const buildPatientTokenValidationQuery = (
   return [gettokenData, tokenLoading, tokenData];
 };
 
-export const buildTherapistTokenValidationQuery = (
-  onCompleted: (therapistData: any) => void
-): [LazyQueryExecFunction<any, OperationVariables>, boolean, any] => {
-  const [gettokenData, { loading: tokenLoading, data: tokenData }] =
-    useLazyQuery(GET_THERAPIST_TOKEN_DATA, {
-      onCompleted: async (data) => {
-        /* istanbul ignore next */
-        if (data.getTokenData) {
-          const user_type: string = data!.getTokenData.user_type;
-          /* istanbul ignore next */
-          if (user_type != "therapist") {
-            window.location.href =
-              "https://" + window.location.hostname + "/account";
-          } else {
-            await onCompleted(data!.getTokenData);
-          }
-        }
-      },
-    });
-  return [gettokenData, tokenLoading, tokenData];
-};
-
 export const buildAdminTokenValidationQuery = (
   onCompleted: (adminData: any) => void
 ): [LazyQueryExecFunction<any, OperationVariables>, boolean, any] => {
