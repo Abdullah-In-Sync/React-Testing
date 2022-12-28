927afafd1ae608c3db2c2762678347aa8e8b0dbc
piyush.singh@myhelp.co.uk <piyush.singh@myhelp.co.uk>
2022-12-28 16:18:05 +0530

    api returning data fix

--- src/graphql/query/patient.tsx
+++ src/graphql/query/patient.tsx
@@ -120,8 +120,6 @@ export const GET_PATIENT_MONITORING_LIST = gql`
       _id
       ca_cat_id
       ca_subcat_id
-      created_date
-      last_completed_date
       monitor_cat_id
       patient_id
       ptmon_frequency
