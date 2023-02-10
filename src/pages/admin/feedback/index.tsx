24a0ccec8fcf153fe37ff7b2f0386b1ffce2d94f
piyush.singh@myhelp.co.uk <piyush.singh@myhelp.co.uk>
2023-02-10 16:15:31 +0530

    test failing fix

--- src/pages/admin/feedback/index.tsx
+++ src/pages/admin/feedback/index.tsx
@@ -373,7 +373,7 @@ const Feedback: NextPage = () => {
   };
   /* istanbul ignore next */
   const handleView = (id) => {
-    router?.push(`/admin/feedback/view/${id}`);
+    router.push(`/admin/feedback/view/${id}`);
   };
   const changePage = (url: any) => {
     /* istanbul ignore next */
