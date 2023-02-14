d0bd439488456f9262cd8e52d48474310c6b5d79
Ravindra Bhanderi <ravinra.bhanderi@myhelp.co.uk>
2023-02-14 21:08:45 +0530

    Feat: fix the style issue

--- src/components/common/SafetyPlan/safetyPlanStyle.tsx
+++ src/components/common/SafetyPlan/safetyPlanStyle.tsx
@@ -1,14 +1,14 @@
-import { makeStyles } from "@mui/styles";
+import { makeStyles } from "@material-ui/core/styles";
 
 export const useStyles = makeStyles({
   headingText: {
-    fontWeight: "700",
+    fontWeight: 700,
     fontSize: "16px",
     lineHeight: "20px",
     color: "#6EC9DB",
   },
   descriptionWrapper: {
-    border: "1px solid rgba(0, 0, 0, 0.2)",
+    border: "1px solid rgba(0, 0, 0,yarn 0.2)",
     borderRadius: "8px",
     padding: "15px",
     marginTop: "10px",
