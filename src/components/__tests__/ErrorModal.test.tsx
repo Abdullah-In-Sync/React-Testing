2e7042ef1e266ca6830cef69a9850080ea35a686
Ravindra Bhanderi <ravinra.bhanderi@myhelp.co.uk>
2023-02-16 09:51:16 +0530

    Feat: fix the linting issye

--- src/components/__tests__/ErrorModal.test.tsx
+++ src/components/__tests__/ErrorModal.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import ErrorModel from "../common/ErrorModal";
 
 describe("when rendered with a `testid` prop", () => {
