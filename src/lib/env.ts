b4815ad0a45dafcd6bcd32d38a7a97b113ec66bd
Myles <myles@myhelp.co.uk>
2023-02-28 15:56:06 +0000

    add BTB Course 1 URL

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -7,6 +7,11 @@ export const env = {
   v1: {
     rootUrl: process.env.NEXT_PUBLIC_V1_ROOT_URL,
   },
+  btb: {
+    course1: {
+      url: process.env.NEXT_PUBLIC_BTB_COURSE_1_URL,
+    }
+  },
   corpWebsite: {
     terms: "https://myhelp.co.uk/terms-of-use.html",
     cookies: "https://myhelp.co.uk/cookies-policy.html",
