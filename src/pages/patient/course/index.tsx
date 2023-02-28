f46be021e2831fcf6129df195060fe173b72268c
piyush.singh@myhelp.co.uk <piyush.singh@myhelp.co.uk>
2023-02-28 10:28:12 +0530

    url change

--- src/pages/patient/course/index.tsx
+++ src/pages/patient/course/index.tsx
@@ -5,9 +5,7 @@ import Course from "../../../components/patient/course/Course";
 import withAuthentication from "../../../hoc/auth";
 
 const b2b_redirect_url =
-  process.env.NEXT_PUBLIC_ENVIRONMENT == "dev"
-    ? null
-    : "https://actions.learning.dev/learner/browse/56b3982f-49e1-362f-b02c-9aec72e33d4c";
+  "https://actions.learning.dev/learner/browse/56b3982f-49e1-362f-b02c-9aec72e33d4c";
 
 const PatientById: NextPage = () => {
   const router = useRouter();
