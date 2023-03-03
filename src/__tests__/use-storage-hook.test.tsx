ebad2551fa23f6efce16a982e2e75e7ac21ef702
Ravindra Bhanderi <ravinra.bhanderi@myhelp.co.uk>
2023-03-03 17:38:04 +0530

    Feat: lint fix

--- src/__tests__/use-storage-hook.test.tsx
+++ src/__tests__/use-storage-hook.test.tsx
@@ -1,7 +1,7 @@
 import { renderHook } from "@testing-library/react";
 import useStorage from "../components/common/hooks/useStorage";
-describe("useStorage hook testing", () => {
 
+describe("useStorage hook testing", () => {
   it("should return getItem function", async () => {
     const {
       result: { current },
