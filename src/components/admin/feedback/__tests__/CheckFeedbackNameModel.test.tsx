aac8aa17938e097b432ce5e79d2c8542220f7dc7
Ravindra Bhanderi <ravinra.bhanderi@myhelp.co.uk>
2023-03-15 15:58:23 +0530

    Feat: fix the type issue

--- src/components/admin/feedback/__tests__/CheckFeedbackNameModel.test.tsx
+++ src/components/admin/feedback/__tests__/CheckFeedbackNameModel.test.tsx
@@ -24,28 +24,27 @@ describe("when rendered with a `title` prop", () => {
       },
     ];
 
-    const validationList = {
-      checkFeedbackName: [
-        {
-          org_id: "2301536c4d674b3598814174d8f19593",
-          organization_name: "resteasy.dev-myhelp",
-          session_no: "1",
-          __typename: "FeedbackName",
-        },
-        {
-          org_id: "2301536c4d674b3598814174d8f19593",
-          organization_name: "resteasy.dev-myhelp",
-          session_no: "2",
-          __typename: "FeedbackName",
-        },
-        {
-          org_id: "2301536c4d674b3598814174d8f19593",
-          organization_name: "resteasy.dev-myhelp",
-          session_no: "3",
-          __typename: "FeedbackName",
-        },
-      ],
-    };
+    const validationList = [
+      {
+        org_id: "2301536c4d674b3598814174d8f19593",
+        organization_name: "resteasy.dev-myhelp",
+        session_no: "1",
+        __typename: "FeedbackName",
+      },
+      {
+        org_id: "2301536c4d674b3598814174d8f19593",
+        organization_name: "resteasy.dev-myhelp",
+        session_no: "2",
+        __typename: "FeedbackName",
+      },
+      {
+        org_id: "2301536c4d674b3598814174d8f19593",
+        organization_name: "resteasy.dev-myhelp",
+        session_no: "3",
+        __typename: "FeedbackName",
+      },
+    ];
+
     render(
       <CheckFeedbackModel
         isOpen={true}
