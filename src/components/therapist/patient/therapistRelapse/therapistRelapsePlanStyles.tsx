470cfb9d2471f20bd31cc60e1a492b8d43b50eee
piyush.singh@myhelp.co.uk <piyush.singh@myhelp.co.uk>
2023-03-17 15:23:50 +0530

    therapist add relapse plan changes

---
+++ src/components/therapist/patient/therapistRelapse/therapistRelapsePlanStyles.tsx
@@ -0,0 +1,24 @@
+import { makeStyles } from "@mui/styles";
+import { Theme } from "@mui/system";
+
+export const useStyles = makeStyles((theme: Theme) => ({
+  formWrapper: {
+    padding: "1.7rem 0 0 0",
+    "& .bottomActionButtonsWrapper": {
+      display: "flex",
+      flexWrap: "wrap",
+      flex: "1",
+      justifyContent: "center",
+      padding: "10px 0px",
+
+      "& .cancelButton": {
+        background: theme.palette.secondary.main,
+      },
+    },
+    "& .fieldsBoxWrapperFirst": {
+      "& .MuiBox-root": {
+        marginBottom: "0.7rem",
+      },
+    },
+  },
+}));
