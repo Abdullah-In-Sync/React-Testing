b7787b880ec0cd0f28d584a93f1949c1923e03ea
piyush.singh@myhelp.co.uk <piyush.singh@myhelp.co.uk>
2023-04-18 18:25:33 +0530

    build config fix

--- src/utility/helper.ts
+++ src/utility/helper.ts
@@ -1,5 +1,7 @@
 export const uniqueString = (): string => {
   const buf = new Uint8Array(1);
-  window.crypto.getRandomValues(buf);
+  if (typeof window !== "undefined") {
+    window.crypto.getRandomValues(buf);
+  }
   return Date.now().toString(36) + buf[0].toString(36).substring(2);
 };
