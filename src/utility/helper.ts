b9136bb1bd03d401e140109fe05776b1f9a0fb1b
piyush.singh@myhelp.co.uk <piyush.singh@myhelp.co.uk>
2023-04-18 18:20:50 +0530

    build config fix try

--- src/utility/helper.ts
+++ src/utility/helper.ts
@@ -1,7 +1,5 @@
-const crypto = require("crypto");
-
 export const uniqueString = (): string => {
   const buf = new Uint8Array(1);
-  crypto.getRandomValues(buf);
+  window.crypto.getRandomValues(buf);
   return Date.now().toString(36) + buf[0].toString(36).substring(2);
 };
