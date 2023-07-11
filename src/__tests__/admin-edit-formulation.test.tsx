7abebb884356dc9b98d8d089a0013e342a556802
1711shub <114855116+1711shub@users.noreply.github.com>
2023-07-11 18:15:56 +0530

    update formulation admin

---
+++ src/__tests__/admin-edit-formulation.test.tsx
@@ -0,0 +1,182 @@
+import { screen, render, waitFor, fireEvent } from "@testing-library/react";
+import { SnackbarProvider } from "notistack";
+import { MockedProvider, MockedResponse } from "@apollo/client/testing";
+import { GET_ADMIN_TOKEN_DATA } from "../graphql/query/common";
+import { useRouter } from "next/router";
+import { useAppContext } from "../contexts/AuthContext";
+import { UPDATE_ADMIN_FORMULATION_BY_ID } from "../graphql/formulation/graphql";
+import EditFormulation from "../pages/admin/resource/editformulation/[id]";
+import { GET_FORMULATION_BY_ID } from "../graphql/query/resource";
+
+jest.mock("next/router", () => ({
+  useRouter: jest.fn(),
+}));
+
+jest.mock("../contexts/AuthContext");
+
+// mocks
+const buildMocks = (): {
+  mocks: MockedResponse[];
+} => {
+  const _mocks: MockedResponse[] = [];
+
+  // fetch token for user query
+  _mocks.push({
+    request: {
+      query: GET_ADMIN_TOKEN_DATA,
+      variables: {},
+    },
+    result: {
+      data: {
+        getTokenData: {
+          _id: "9ea296b4-4a19-49b6-9699-c1e2bd6fc946",
+          user_type: "admin",
+          parent_id: "73ddc746-b473-428c-a719-9f6d39bdef81",
+          perm_ids: "9,10,14,21,191,65,66",
+          user_status: "1",
+          created_date: "2021-12-20 16:20:55",
+          updated_date: "2021-12-20 16:20:55",
+        },
+      },
+    },
+  });
+
+  // fetch formulation list query
+  _mocks.push({
+    request: {
+      query: GET_FORMULATION_BY_ID,
+      variables: { formulation_id: "750a6993f61d4e58917e31e1244711f5" },
+    },
+    result: {
+      data: {
+        getFormulationById: {
+          _id: "f771c319-aba6-414c-ba4d-b5e55a4ed6d7",
+          created_date: "2023-07-07T05:39:53.551Z",
+          download_formulation_url:
+            "https://myhelp-dev-webapp-s3-eu-west-1-673928258113.s3.eu-west-1.amazonaws.com/formulation/070622248__Screenshot%202023-03-16%20at%205.21.17%20PM.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=ASIAZZ2KBEJAWT4HUBJN%2F20230711%2Feu-west-1%2Fs3%2Faws4_request&X-Amz-Date=20230711T065523Z&X-Amz-Expires=1800&X-Amz-Security-Token=IQoJb3JpZ2luX2VjED8aCWV1LXdlc3QtMSJHMEUCIAE66%2BvYJHbJvqIjK%2BDyCI3luNI2KUi2DLpE0koze%2FrSAiEAvsaP7oqB0ljpGP058BqhLnSUg5KFSstqeH9gV3Cgmz8q7QIIuP%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FARABGgw2NzM5MjgyNTgxMTMiDNHve%2F4lJzpLh7KR1SrBApObgLuO9E1QCaefPd77MpvXAk9Qo%2BhNF0oevH3FIo1tAiJl2oG6jWTHocqyqYRkXlUINqTaLStyRg6BzJBB2jxZg3ajyvVso4HpGSA73QeaBLPd3TSLUe%2B9K%2F3dVbYBOKgRdX4wR1gKaU5XBZKfh%2FLcYtD44vOw4igbgZ7J6Ut7QrLJSQEsxoglsyZgBMFohC87NGQUmEdNNTSyIBqp3%2BGig2I%2ByH0saNvJEsSGc7RN6WIC9DQipsT2A4nn2HtnKmDh42yLnyVHJCCuGL04%2FTMNz%2FO0pHoS9pA6dplxIKH6cv4mE4nMLhfLdsm0TavBz14UDHYP%2Bl4hXnl%2BA0rMctv8Od1CsBspmfcgYTZORHnvNpjqnPIuXDOW0Y8D4kVNipK1b9sNLMCoT54tmyd3m0mVxeVBEV2oi%2BnqRd35N6YjpDDm9LOlBjqeAXxU0wKlQnWQL%2FSX0Z0RLJArblYG%2Fo2Q%2FPSNSCFpUf3Ldo6IGGQOKwHXhMw9avgSbG8x0yC8fK6IfAwrlRH%2F5S%2BnncdOyKq2dOKzTSk6X%2BkOw7qGV4lX5wmzdAY7%2F1kyhFwC92FWrW7%2Bw8Lum6X7zT1fmj8PQRPs%2FLe07VAtSC1FiL3xbI%2F%2F5YScaGI45NBkTomK8SBSIlDyFcF4MdHN&X-Amz-Signature=b680234962e314d5a33970ab24b9ec7c64a67bc08277d67895c7474083d545fa&X-Amz-SignedHeaders=host&response-content-disposition=attachment%3B%20filename%3D070622248__Screenshot%202023-03-16%20at%205.21.17%20PM.png&x-id=GetObject",
+          fav_for_detail: null,
+          formulation_avail_for: '"\\"[1, 2]\\""',
+          formulation_desc: "Punjab 512",
+          formulation_img: "070622248__Screenshot 2023-03-16 at 5.21.17 PM.png",
+          formulation_instruction: "Punjab 512",
+          formulation_name: "Punjab 512",
+          formulation_status: 1,
+          formulation_type: 1,
+          formulation_url:
+            "https://myhelp-dev-webapp-s3-eu-west-1-673928258113.s3.eu-west-1.amazonaws.com/formulation/070622248__Screenshot%202023-03-16%20at%205.21.17%20PM.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=ASIAZZ2KBEJAWT4HUBJN%2F20230711%2Feu-west-1%2Fs3%2Faws4_request&X-Amz-Date=20230711T065523Z&X-Amz-Expires=1800&X-Amz-Security-Token=IQoJb3JpZ2luX2VjED8aCWV1LXdlc3QtMSJHMEUCIAE66%2BvYJHbJvqIjK%2BDyCI3luNI2KUi2DLpE0koze%2FrSAiEAvsaP7oqB0ljpGP058BqhLnSUg5KFSstqeH9gV3Cgmz8q7QIIuP%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FARABGgw2NzM5MjgyNTgxMTMiDNHve%2F4lJzpLh7KR1SrBApObgLuO9E1QCaefPd77MpvXAk9Qo%2BhNF0oevH3FIo1tAiJl2oG6jWTHocqyqYRkXlUINqTaLStyRg6BzJBB2jxZg3ajyvVso4HpGSA73QeaBLPd3TSLUe%2B9K%2F3dVbYBOKgRdX4wR1gKaU5XBZKfh%2FLcYtD44vOw4igbgZ7J6Ut7QrLJSQEsxoglsyZgBMFohC87NGQUmEdNNTSyIBqp3%2BGig2I%2ByH0saNvJEsSGc7RN6WIC9DQipsT2A4nn2HtnKmDh42yLnyVHJCCuGL04%2FTMNz%2FO0pHoS9pA6dplxIKH6cv4mE4nMLhfLdsm0TavBz14UDHYP%2Bl4hXnl%2BA0rMctv8Od1CsBspmfcgYTZORHnvNpjqnPIuXDOW0Y8D4kVNipK1b9sNLMCoT54tmyd3m0mVxeVBEV2oi%2BnqRd35N6YjpDDm9LOlBjqeAXxU0wKlQnWQL%2FSX0Z0RLJArblYG%2Fo2Q%2FPSNSCFpUf3Ldo6IGGQOKwHXhMw9avgSbG8x0yC8fK6IfAwrlRH%2F5S%2BnncdOyKq2dOKzTSk6X%2BkOw7qGV4lX5wmzdAY7%2F1kyhFwC92FWrW7%2Bw8Lum6X7zT1fmj8PQRPs%2FLe07VAtSC1FiL3xbI%2F%2F5YScaGI45NBkTomK8SBSIlDyFcF4MdHN&X-Amz-Signature=05c85ce7552f8f4b96084ec35ca12d3431970b32eccfe28dabec05ac2635650d&X-Amz-SignedHeaders=host&response-content-disposition=inline%3B%20filename%3D070622248__Screenshot%202023-03-16%20at%205.21.17%20PM.png&x-id=GetObject",
+          org_id: "d1f2bbd3-3388-4ca2-9d68-55b95574a269",
+          template_data: "",
+          template_id: "",
+          updated_date: "2023-07-11T06:22:40.578Z",
+          user_id: "9ea296b4-4a19-49b6-9699-c1e2bd6fc946",
+          __typename: "AdminFormulationData",
+        },
+      },
+    },
+  });
+
+  _mocks.push({
+    request: {
+      query: UPDATE_ADMIN_FORMULATION_BY_ID,
+      variables: {
+        formulation_id: "750a6993f61d4e58917e31e1244711f5",
+        updateFormulation: {
+          formulation_desc: "Punjab 512",
+          formulation_instruction: "Punjab 512",
+          formulation_name: "test",
+          org_id: "d1f2bbd3-3388-4ca2-9d68-55b95574a269",
+          formulation_img: undefined,
+          formulation_avail_for: '"\\"\\\\\\"[1, 2]\\\\\\"\\""',
+        },
+      },
+    },
+    result: {
+      data: {
+        updateFormulationById: {
+          _id: "f771c319-aba6-414c-ba4d-b5e55a4ed6d7",
+          created_date: "2023-07-07T05:39:53.551Z",
+          download_formulation_url:
+            "https://myhelp-dev-webapp-s3-eu-west-1-673928258113.s3.eu-west-1.amazonaws.com/formulation/070622248__Screenshot%202023-03-16%20at%205.21.17%20PM.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=ASIAZZ2KBEJA3GHWP2OJ%2F20230711%2Feu-west-1%2Fs3%2Faws4_request&X-Amz-Date=20230711T075124Z&X-Amz-Expires=1800&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEEAaCWV1LXdlc3QtMSJGMEQCIH4ff4uBqCBbCHxX2v0rA9Aw4f8PtX1%2B3ctg9BbiRJ4FAiBX8KT09OpYBbycqfBIq%2Fj3aQ%2BuzV6ygWIU%2F5WDtumSsSrtAgi5%2F%2F%2F%2F%2F%2F%2F%2F%2F%2F8BEAEaDDY3MzkyODI1ODExMyIMfcDpac6v5rFSIk8PKsECqYIb0goE1xHGRAGcgJSZ3Lt0TdQCLTTwoB73JoPXKF5jV%2BWTfdn%2Fotb16wMs%2BVfnOt3cn8tKdXn18szg7GTQ4CHVgJw1DUkJfffWPW1kcF%2FNgNAMZJN%2BcochqL052LhyAYW%2BxVoBwCZv8BbZuzUvmRXCNIdwYv7KfDru%2FhasASXFacWCbCkc2vhku2I1ONyLhc61TH9QX%2BAeQ95Z3yivXPX9Qmup%2B0C5hM3b055wLHCIY%2FnUTCTwkcJ3rmFqkHvZr9UeRHBUG33JWhdpRdUylWotMld2alPuUt7UnYtQfkloLTQzXx6a%2BrRei9DZe81rGghEKe9Wg0vsmVApvbw%2FVdRXNYHSejjOOU5%2BbJVsjIBj3rTGCebYng%2B5S1aGn8SRyAHU3NzaluGM9UKGLMR02YxSLVYIUFXfYdlmIZv3hsH8MOiTtKUGOp8BMlhSpJhVBkXhdsjcyaNVK6QJsCE%2FHkdyyyfivvOKyyFM4taX8PuOnDHVg9MPxiupOnH7yrgxmOn4VX2NSiRqxoxumI5LV3qc7shzn8KI2V6VefFTgT%2BZeZxe1DMPe4A0%2BalMvN6iL8fRRgJAJpdz0%2FmfvjNITrB1Lp6l8PyyN1JjRAtC8KeZ%2BrFr%2BMBPaQsdKTCzsQcG2aqCNS6aRIFb&X-Amz-Signature=65632de52be048a8d669d09cbfa687356d99a2a9dd5653cf02ac95c040c3e622&X-Amz-SignedHeaders=host&response-content-disposition=attachment%3B%20filename%3D070622248__Screenshot%202023-03-16%20at%205.21.17%20PM.png&x-id=GetObject",
+          formulation_avail_for: '"\\"\\\\\\"[1, 2]\\\\\\"\\""',
+          formulation_desc: "Punjab 512",
+          formulation_img: "070622248__Screenshot 2023-03-16 at 5.21.17 PM.png",
+          formulation_instruction: "Punjab 512",
+          formulation_returnurl: null,
+          formulation_name: "ABCD",
+          formulation_status: 1,
+          formulation_type: 1,
+          formulation_url:
+            "https://myhelp-dev-webapp-s3-eu-west-1-673928258113.s3.eu-west-1.amazonaws.com/formulation/070622248__Screenshot%202023-03-16%20at%205.21.17%20PM.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=ASIAZZ2KBEJA3GHWP2OJ%2F20230711%2Feu-west-1%2Fs3%2Faws4_request&X-Amz-Date=20230711T075124Z&X-Amz-Expires=1800&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEEAaCWV1LXdlc3QtMSJGMEQCIH4ff4uBqCBbCHxX2v0rA9Aw4f8PtX1%2B3ctg9BbiRJ4FAiBX8KT09OpYBbycqfBIq%2Fj3aQ%2BuzV6ygWIU%2F5WDtumSsSrtAgi5%2F%2F%2F%2F%2F%2F%2F%2F%2F%2F8BEAEaDDY3MzkyODI1ODExMyIMfcDpac6v5rFSIk8PKsECqYIb0goE1xHGRAGcgJSZ3Lt0TdQCLTTwoB73JoPXKF5jV%2BWTfdn%2Fotb16wMs%2BVfnOt3cn8tKdXn18szg7GTQ4CHVgJw1DUkJfffWPW1kcF%2FNgNAMZJN%2BcochqL052LhyAYW%2BxVoBwCZv8BbZuzUvmRXCNIdwYv7KfDru%2FhasASXFacWCbCkc2vhku2I1ONyLhc61TH9QX%2BAeQ95Z3yivXPX9Qmup%2B0C5hM3b055wLHCIY%2FnUTCTwkcJ3rmFqkHvZr9UeRHBUG33JWhdpRdUylWotMld2alPuUt7UnYtQfkloLTQzXx6a%2BrRei9DZe81rGghEKe9Wg0vsmVApvbw%2FVdRXNYHSejjOOU5%2BbJVsjIBj3rTGCebYng%2B5S1aGn8SRyAHU3NzaluGM9UKGLMR02YxSLVYIUFXfYdlmIZv3hsH8MOiTtKUGOp8BMlhSpJhVBkXhdsjcyaNVK6QJsCE%2FHkdyyyfivvOKyyFM4taX8PuOnDHVg9MPxiupOnH7yrgxmOn4VX2NSiRqxoxumI5LV3qc7shzn8KI2V6VefFTgT%2BZeZxe1DMPe4A0%2BalMvN6iL8fRRgJAJpdz0%2FmfvjNITrB1Lp6l8PyyN1JjRAtC8KeZ%2BrFr%2BMBPaQsdKTCzsQcG2aqCNS6aRIFb&X-Amz-Signature=02e8e8029fe119cd358518246b67d54bf8e3ae4c8ab712a516ccb55afbb7f7e7&X-Amz-SignedHeaders=host&response-content-disposition=inline%3B%20filename%3D070622248__Screenshot%202023-03-16%20at%205.21.17%20PM.png&x-id=GetObject",
+          updated_date: "2023-07-11T07:51:24.501Z",
+          user_id: "9ea296b4-4a19-49b6-9699-c1e2bd6fc946",
+          __typename: "FormulationData",
+        },
+      },
+    },
+  });
+
+  return { mocks: _mocks };
+};
+
+const { mocks } = buildMocks();
+const sut = async () => {
+  render(
+    <MockedProvider mocks={mocks}>
+      <SnackbarProvider>
+        <EditFormulation />
+      </SnackbarProvider>
+    </MockedProvider>
+  );
+};
+describe(" Formulation page", () => {
+  beforeEach(() => {
+    const mockRouter = {
+      push: jest.fn(),
+    };
+
+    (useRouter as jest.Mock).mockReturnValue(mockRouter);
+    (useAppContext as jest.Mock).mockReturnValue({
+      isAuthenticated: true,
+      user: {
+        _id: "9ea296b4-4a19-49b6-9699-c1e2bd6fc946",
+        user_type: "admin",
+        parent_id: "73ddc746-b473-428c-a719-9f6d39bdef81",
+        perm_ids: "9,10,14,21,191,65,66",
+        user_status: "1",
+        created_date: "2021-12-20 16:20:55",
+        updated_date: "2021-12-20 16:20:55",
+      },
+    });
+  });
+  // check for Patient Session Resource list
+
+  it.only("should render complete edit formulation form and submit with valid data", async () => {
+    (useRouter as jest.Mock).mockImplementation(() => ({
+      query: {
+        id: "750a6993f61d4e58917e31e1244711f5",
+      },
+    }));
+    await sut();
+
+    await waitFor(async () => {
+      expect(screen.getByTestId("formulation_name")).toHaveValue("Punjab 512");
+
+      fireEvent.change(screen.queryByTestId("formulation_name"), {
+        target: { value: "test" },
+      });
+
+      await waitFor(async () => {
+        fireEvent.click(screen.queryByTestId("editFormulationSubmitButton"));
+        expect(screen.getByTestId("confirmButton")).toBeInTheDocument();
+
+        fireEvent.click(screen.queryByTestId("confirmButton"));
+      });
+
+      await waitFor(async () => {
+        expect(
+          screen.getByText("Formulation updated successfully!")
+        ).toBeInTheDocument();
+      });
+    });
+  });
+});
---
+++ src/components/common/EditFormulationForm/EditFormulationForm.tsx
@@ -0,0 +1,360 @@
+import React, { FormEvent, useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import {
+  Box,
+  Button,
+  Checkbox,
+  FormControlLabel,
+  FormLabel,
+  Grid,
+} from "@mui/material";
+
+import { useLazyQuery, useQuery } from "@apollo/client";
+
+import {
+  GET_FORMULATION_BY_ID,
+  GET_UPLOAD_LOGO_URL,
+} from "../../../graphql/query/resource";
+import { useSnackbar } from "notistack";
+import TextFieldComponent from "../../common/TextField/TextFieldComponent";
+import SingleSelectComponent from "../../common/SelectBox/SingleSelect/SingleSelectComponent";
+import UploadButtonComponent from "../../common/UploadButton/UploadButtonComponent";
+import { getUpdatedFileName, uploadToS3 } from "../../../lib/helpers/s3";
+import { editFormulationFormField } from "../../../utility/types/resource_types";
+import Link from "@mui/material/Link";
+import Loader from "../Loader";
+import { useAppContext } from "../../../contexts/AuthContext";
+import { GET_ORG_DATA } from "../../../graphql/query";
+import ConfirmationModal from "../ConfirmationModal";
+
+const defaultFormValue = {
+  _id: "",
+  formulation_name: "",
+  formulation_type: 2,
+  formulation_status: 1,
+  org_id: "",
+  formulation_desc: "",
+  formulation_instruction: "",
+  formulation_url: null,
+  formulation_avail_for: "",
+  formulation_img: "",
+  download_formulation_url: "",
+  file_name: "invalid.pdf",
+  uploadFile: null,
+};
+
+/* istanbul ignore next */
+type propTypes = {
+  resourceType: "edit" | "update";
+  onSubmit?: any;
+  setLoader: any;
+};
+
+export default function EditFormFormulation(props: propTypes) {
+  const {
+    user: { user_type: userType, therapist_data: { org_id: orgId = "" } = {} },
+  } = useAppContext();
+  const router = useRouter();
+  const id = router?.query.id as string;
+  const { enqueueSnackbar } = useSnackbar();
+
+  const [formFields, setFormFields] = useState<editFormulationFormField>({
+    ...defaultFormValue,
+    org_id: orgId,
+  });
+
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [loader, setLoader] = useState<boolean>(false);
+  const [isConfirm, setIsConfirm] = useState(false);
+
+  const [getOrgData, { data: orgData }] = useLazyQuery(GET_ORG_DATA, {
+    onCompleted: () => {
+      props.setLoader(false);
+    },
+  });
+
+  const { data: uploadFormulationURL } = useQuery(GET_UPLOAD_LOGO_URL, {
+    variables: {
+      fileName: formFields.file_name,
+      imageFolder: "images",
+    },
+  });
+
+  //FOR PREFILLED INPUT
+  const [getFormulationData, { data: resourceData }] = useLazyQuery(
+    GET_FORMULATION_BY_ID,
+    {
+      onCompleted: (data) => {
+        console.log("Koca: data ", data);
+        setLoader(false);
+      },
+    }
+  );
+
+  useEffect(() => {
+    props.setLoader(true);
+    if (userType == "admin") {
+      getOrgData();
+    }
+  }, []);
+
+  useEffect(() => {
+    /* istanbul ignore next */
+    setLoader(true);
+    getFormulationData({
+      variables: { formulation_id: id },
+    });
+
+    // add resource data to the state to prefilled value
+    const data = resourceData?.getFormulationById;
+    if (data) {
+      setFormFields(data);
+    }
+    setLoader(false);
+  }, [resourceData]);
+
+  const set2 = (
+    e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
+  ) => {
+    const fieldName = e.target.name;
+    const value = e.target.value;
+
+    setFormFields((oldValues) => ({ ...oldValues, [fieldName]: value }));
+  };
+
+  const fileOnChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    /* istanbul ignore next */
+    const fileObj = event.target.files && event.target.files[0];
+    const { fileName } = getUpdatedFileName(event.target.files[0]);
+    props.setLoader(true);
+    /* istanbul ignore next */
+    if (!fileName) {
+      return;
+    }
+    /* istanbul ignore next */
+    setSelectedFile(fileObj);
+    setFormFields((oldValues) => ({ ...oldValues, ["file_name"]: fileName }));
+    props.setLoader(false);
+  };
+
+  /* istanbul ignore next */
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!formFields.formulation_avail_for) {
+      enqueueSnackbar("Please select availability of resource", {
+        variant: "error",
+        autoHideDuration: 2000,
+      });
+      return;
+    }
+    setIsConfirm(true);
+
+    if (!isConfirm) return;
+  };
+
+  const uploadFileFormulation = async () => {
+    try {
+      props.setLoader(true);
+      if (!uploadFormulationURL?.getFileUploadUrl?.upload_file_url) {
+        props.onSubmit(formFields);
+      } else {
+        if (
+          !(await uploadToS3(
+            selectedFile,
+            uploadFormulationURL?.getFileUploadUrl?.upload_file_url
+          ))
+        ) {
+          throw new Error("There is an error with file upload!");
+        }
+        props.onSubmit(formFields);
+      }
+    } catch (e) {
+      enqueueSnackbar(e.message, {
+        variant: "error",
+        autoHideDuration: 2000,
+      });
+    } finally {
+      props.setLoader(false);
+    }
+  };
+
+  const clearIsConfirmCancel = () => {
+    /* istanbul ignore next */
+    setIsConfirm(false);
+  };
+
+  return (
+    <>
+      <Loader visible={loader} />
+
+      <Box
+        sx={{ flexGrow: 1, border: "1px solid #cecece" }}
+        p={5}
+        borderRadius="7px"
+      >
+        <>
+          <form onSubmit={handleSubmit} data-testid="formulation-edit-form">
+            <Grid container spacing={2} marginBottom={5}>
+              <Grid item xs={4}>
+                <TextFieldComponent
+                  required={true}
+                  name="formulation_name"
+                  id="formulation_name"
+                  label="Name"
+                  value={formFields?.formulation_name}
+                  onChange={set2}
+                  fullWidth={true}
+                  inputProps={{ "data-testid": "formulation_name" }}
+                  variant="outlined"
+                  className="form-control-bg"
+                  size="small"
+                />
+              </Grid>
+
+              <Grid item xs={4}>
+                <SingleSelectComponent
+                  fullWidth={true}
+                  required={true}
+                  id="resourceOrgSelect"
+                  labelId="resourceOrg"
+                  name="org_id"
+                  value={formFields?.org_id}
+                  label="Select Organization"
+                  onChange={set2}
+                  inputProps={{ "data-testid": "org_id" }}
+                  options={(orgData && orgData?.getOrganizationData) || []}
+                  mappingKeys={["_id", "name"]}
+                  size="small"
+                  className="form-control-bg"
+                  disabled={true}
+                />
+              </Grid>
+            </Grid>
+
+            <Grid container spacing={2} marginBottom={5}>
+              <Grid item xs={12}>
+                <TextFieldComponent
+                  name="formulation_desc"
+                  id="descrption"
+                  label="Description"
+                  value={formFields?.formulation_desc}
+                  multiline
+                  rows={4}
+                  onChange={set2}
+                  inputProps={{ "data-testid": "formulation_desc" }}
+                  fullWidth={true}
+                  className="form-control-bg"
+                />
+              </Grid>
+            </Grid>
+
+            <Grid container spacing={2} marginBottom={5}>
+              <Grid item xs={12}>
+                <TextFieldComponent
+                  name="formulation_instruction"
+                  id="instructions"
+                  label="Instructions"
+                  value={formFields?.formulation_instruction}
+                  multiline
+                  rows={4}
+                  onChange={set2}
+                  inputProps={{ "data-testid": "formulation_instruction" }}
+                  fullWidth={true}
+                  className="form-control-bg"
+                />
+              </Grid>
+            </Grid>
+
+            <Grid container spacing={2} marginBottom={5}>
+              <Grid item xs={7}>
+                Upload Resource :
+                <Link
+                  data-testid="edit-upload-file"
+                  href={
+                    resourceData?.getFormulationById?.download_formulation_url
+                  }
+                  underline="none"
+                  target="_blank"
+                >
+                  {resourceData?.getFormulationById?.formulation_img}
+                </Link>
+              </Grid>
+
+              <Grid item xs={7}>
+                <Box display="flex" alignItems="center">
+                  <UploadButtonComponent
+                    variant="contained"
+                    name="RESOURCE_FILENAME"
+                    inputProps={{ "data-testid": "resource_file_upload" }}
+                    onChange={fileOnChange}
+                    fileName={selectedFile?.name}
+                  />
+                </Box>
+              </Grid>
+            </Grid>
+
+            <Grid container spacing={2} marginBottom={5}>
+              <Grid item xs={12}>
+                <Box>
+                  <FormLabel required={true} sx={{ mr: 1 }}>
+                    Select the Availability of Resource:
+                  </FormLabel>
+                  <FormControlLabel
+                    control={
+                      <Checkbox
+                        disabled={true}
+                        checked={
+                          formFields.formulation_avail_for?.includes(1) || false
+                        }
+                        name="allTherapists"
+                        value={1}
+                      />
+                    }
+                    label="All Therapists"
+                  />
+                  <FormControlLabel
+                    data-testid="resource_avail_onlyme"
+                    control={
+                      <Checkbox
+                        disabled={true}
+                        checked={
+                          formFields.formulation_avail_for?.includes(2) || false
+                        }
+                        name="onlyMe"
+                        value={2}
+                      />
+                    }
+                    label="Only me"
+                  />
+                </Box>
+              </Grid>
+            </Grid>
+
+            <Grid container spacing={2} marginBottom={5}>
+              <Grid item xs={12} textAlign="center">
+                <Button
+                  data-testid="editFormulationSubmitButton"
+                  variant="contained"
+                  type="submit"
+                >
+                  Save
+                </Button>
+              </Grid>
+            </Grid>
+          </form>
+        </>
+      </Box>
+      {isConfirm && (
+        <ConfirmationModal
+          label="Are you sure you want to update this formulation?"
+          onCancel={clearIsConfirmCancel}
+          onConfirm={() => {
+            /* istanbul ignore next */
+            uploadFileFormulation();
+          }}
+        />
+      )}
+    </>
+  );
+}
