089a015fab4922e50c39faf62a8e61728a5b2a9f
piyushmy <piyush.singh@myhelp.co.uk>
2023-07-14 09:18:33 +0530

    image style removed

--- src/components/common/Image/Image.tsx
+++ src/components/common/Image/Image.tsx
@@ -6,14 +6,7 @@ interface Props {
 const CustomImage: React.FC<Props> = ({ url }) => {
   return (
     <>
-      <img
-        alt="no image"
-        src={url}
-        style={{
-          width: "100%",
-          height: "auto",
-        }}
-      />
+      <img alt="no image" src={url} />
     </>
   );
 };
