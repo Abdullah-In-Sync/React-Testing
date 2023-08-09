e428bc7020f09b9d69be93896b0201b5e6b99dcd
Myles <myles.harding@bp.com>
2023-08-09 10:34:04 +0100

    add new env vars

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -12,6 +12,11 @@ export const env = {
       url: process.env.NEXT_PUBLIC_BTB_COURSE_1_URL,
     },
   },
+  cognito: {
+    region: process.env.NEXT_PUBLIC_COGNITO_REGION,
+    userPoolId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_ID,
+    clientId: process.env.NEXT_PUBLIC_COGNITO_APP_CLIENT_ID,
+  },
   rollbar: {
     accessToken: "4f3a122536834cf18716cbf016d2f837",
   },
