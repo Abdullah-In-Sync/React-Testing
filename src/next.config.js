bdf9d0d34fce2131e55ced4bc18d852f87c2da92
Myles <myles.harding@bp.com>
2023-08-14 08:51:02 +0100

    Update next.config.js

--- src/next.config.js
+++ src/next.config.js
@@ -1,21 +1,7 @@
 const nextConfig = {
   reactStrictMode: false,
   trailingSlash: true,
-  basePath: "/v2",
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/v2",
-        basePath: false,
-        permanent: false,
-      },
-    ];
-  },
-  images: {
-    loader: "akamai",
-    path: "/v2",
-  },
+  basePath: "/",
 };
 
 module.exports = nextConfig;
