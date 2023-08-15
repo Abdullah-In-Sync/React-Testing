c5d603b6e2b75bf534bb96c927e2ea1f8ded7225
piyushmy <piyush.singh@myhelp.co.uk>
2023-08-15 19:19:35 +0530

    fluctuation fix and public route added

--- src/hoc/routeGaurd.ts
+++ src/hoc/routeGaurd.ts
@@ -1,29 +1,38 @@
-import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
-import { getSessionToken } from "../utility/storage";
 import { homeRoute } from "../lib/constants";
+import { getSessionToken } from "../utility/storage";
+import { isAuth } from "../utility/helper";
 
 export { RouteGuard };
 
 function RouteGuard({ children }) {
   const router = useRouter();
-  const [authorized, setAuthorized] = useState(false);
+
+  const [authorized, setAuthorized] = useState(isAuth(router));
 
   useEffect(() => {
     authCheck(router.asPath);
-    const hideContent = () => setAuthorized(false);
+    const hideContent = () => setAuthorized(isAuth(router));
+
     router.events.on("routeChangeStart", hideContent);
     router.events.on("routeChangeComplete", authCheck);
     return () => {
       router.events.off("routeChangeStart", hideContent);
       router.events.off("routeChangeComplete", authCheck);
     };
-  }, []);
+  }, [router.asPath.split("?")[0].split("/")[1]]);
 
   function authCheck(url) {
     const { userToken, userType } = getSessionToken();
-    const publicPaths = ["/login", "/login/"];
+    const publicPaths = [
+      "/login",
+      "/login/",
+      "/forgotPassword/",
+      "/forgotPassword/[username]",
+      "/forgotPassword/[username]/[code]",
+    ];
     const path = url.split("?")[0];
     if (!userToken && !publicPaths.includes(path)) {
       setAuthorized(false);
