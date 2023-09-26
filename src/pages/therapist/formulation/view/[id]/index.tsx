49011df791871062a039d5b317d48c1a72949962
ravindrabhanderi14 <117576691+ravindrabhanderi14@users.noreply.github.com>
2023-09-26 14:43:36 +0530

    Feat: added therapist formulation view page

---
+++ src/pages/therapist/formulation/view/[id]/index.tsx
@@ -0,0 +1,56 @@
+import { useLazyQuery } from "@apollo/client";
+import type { NextPage } from "next";
+import { useRouter } from "next/router";
+import { useEffect, useRef, useState } from "react";
+import ViewFormulation from "../../../../../components/admin/formulation/view/ViewFormulation";
+import ContentHeader from "../../../../../components/common/ContentHeader";
+import { ModalElement } from "../../../../../components/common/CustomModal/CommonModal";
+import Loader from "../../../../../components/common/Loader";
+import Layout from "../../../../../components/layout";
+import { GET_FORMULATION_BY_ID } from "../../../../../graphql/formulation/graphql";
+import { FormulationData } from "../../../../../graphql/formulation/types";
+
+const ViewFormulationPage: NextPage = () => {
+  const router = useRouter();
+  const { query: { id: formulation_id } = {} } = router;
+  const [loader, setLoader] = useState<boolean>(true);
+  const infoModalRef = useRef<ModalElement>(null);
+
+  const [
+    getFormulation,
+    { data: { getFormulationById: formulationData = undefined } = {} },
+  ] = useLazyQuery<FormulationData>(GET_FORMULATION_BY_ID, {
+    fetchPolicy: "cache-and-network",
+    onCompleted: () => {
+      setLoader(false);
+    },
+  });
+
+  useEffect(() => {
+    getFormulation({
+      variables: { formulation_id },
+    });
+  }, []);
+
+  const backButtonClick = () => {
+    router.back();
+  };
+
+  return (
+    <>
+      <Layout>
+        <Loader visible={loader} />
+        <ContentHeader title="Formulation Detail" />
+        {formulationData && (
+          <ViewFormulation
+            data={formulationData}
+            infoModalRef={infoModalRef}
+            backButtonClick={backButtonClick}
+          />
+        )}
+      </Layout>
+    </>
+  );
+};
+
+export default ViewFormulationPage;
