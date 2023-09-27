50610de108653b6e31149ae9e33a4c0ccd4ad96c
1711shub <114855116+1711shub@users.noreply.github.com>
2023-09-27 09:15:54 +0530

    Fix assessment completly

--- src/components/admin/assessement/createAssessment/FormAssessment.tsx
+++ src/components/admin/assessement/createAssessment/FormAssessment.tsx
@@ -1,122 +1,81 @@
-import React, { useState } from "react";
-import { Button, Grid, Stack, Checkbox } from "@mui/material";
-import { Form } from "formik";
-import { Box, Typography } from "@material-ui/core";
+/* istanbul ignore file */
+import React, { FormEvent, useState } from "react";
+import { Button, Grid, Stack } from "@mui/material";
+import { Box } from "@material-ui/core";
 import TextFieldComponent from "../../../common/TextField/TextFieldComponent";
-import InputLabel from "@mui/material/InputLabel";
-import MenuItem from "@mui/material/MenuItem";
-import FormControl from "@mui/material/FormControl";
-import ListItemText from "@mui/material/ListItemText";
-import Select from "@mui/material/Select";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import { useSnackbar } from "notistack";
+import MultiSelectComponent from "../../../common/SelectBox/MultiSelect/MutiSelectComponent";
+import { addAdminAssessment } from "../../../../utility/types/resource_types";
 
-interface Organization {
-  _id: string;
-  name: string;
-}
+const defaultFormValue = {
+  name: "",
+  org_id: "",
+};
 
 interface ViewProps {
   buttonClick?: (value: any) => void;
-  onPressSubmit?: () => void;
-  organizationList?: Organization[];
-  setPlanId?: any;
-  onChangePlanId?: any;
-  onAssessmentNameChange?: any;
+  organizationList?: any;
+  setSendFormData?: any;
 }
-/* istanbul ignore next */
-const CheckboxAll = ({
-  isAllSelected,
-  onChange,
-}: {
-  isAllSelected: boolean;
-  onChange: () => void;
-}) => (
-  <MenuItem value="all">
-    <ListItemIcon>
-      <Checkbox
-        checked={isAllSelected}
-        indeterminate={!isAllSelected}
-        onChange={onChange}
-        data-testid={`checkbox123`}
-      />
-    </ListItemIcon>
-    <Typography>Select All</Typography>
-  </MenuItem>
-);
-/* istanbul ignore next */
-const OrganizationCheckbox = ({
-  option,
-  checked,
-  onChange,
-}: {
-  option: Organization;
-  checked: boolean;
-  onChange: any;
-}) => (
-  <MenuItem key={option._id} value={option._id}>
-    <ListItemIcon>
-      <Checkbox
-        data-testid={`checkbox12345${option._id}`}
-        checked={checked}
-        onChange={onChange}
-      />
-    </ListItemIcon>
-    <ListItemText primary={option.name} />
-  </MenuItem>
-);
 
 const FormAssessmentBox: React.FC<ViewProps> = ({
-  onPressSubmit,
   organizationList,
-  onChangePlanId,
-  onAssessmentNameChange,
+  setSendFormData,
 }) => {
-  const { enqueueSnackbar } = useSnackbar();
-  const [patientInputs, setpatientInputs] = useState<string>("");
-  const [selected, setSelected] = useState<string[]>([]);
+  const csvDecode = (csvString) => (csvString ? csvString.split(",") : []);
+  const [formFields, setFormFields] = useState<addAdminAssessment>({
+    ...defaultFormValue,
+  });
+  console.log("Koca: formFields ", formFields);
 
-  /* istanbul ignore file */
-  const isAllSelected =
-    organizationList.length > 0 && selected.length === organizationList.length;
+  /* istanbul ignore next */
+  const handleChange = (event) => {
+    const value = event.target.value as string[];
 
-  /* istanbul ignore file */
-  const handleCheckboxChange = (value) => {
-    const newVal = [...selected, value];
-    setSelected(newVal);
-    onChangePlanId(newVal);
-  };
+    if (value[value.length - 1] === "all") {
+      const updatedSelected = [
+        "all",
+        // eslint-disable-next-line no-unsafe-optional-chaining
+        ...organizationList?.map((org) => org._id),
+      ];
 
-  /* istanbul ignore file */
-  const handleAllCheckboxChange = () => {
-    const updatedSelected = isAllSelected
-      ? []
-      : organizationList.map((org) => org._id);
-
-    setSelected(updatedSelected);
-    onChangePlanId(updatedSelected);
-  };
-
-  /* istanbul ignore file */
-  const handlePatientInputChange = (value: string) => {
-    setpatientInputs(value);
-    onAssessmentNameChange(value);
-  };
-
-  /* istanbul ignore file */
-  const handleSubmit = () => {
-    if (patientInputs && selected) {
-      onPressSubmit?.();
+      setFormFields((oldValues) => ({
+        ...oldValues,
+        org_id: updatedSelected.join(","),
+      }));
+      return;
+    } else if (
+      value.length === organizationList?.length &&
+      value?.indexOf("all") === -1
+    ) {
+      setFormFields((oldValues) => ({
+        ...oldValues,
+        org_id: "",
+      }));
     } else {
-      enqueueSnackbar("Please enter assessment name", {
-        variant: "error",
+      setFormFields({
+        ...formFields,
+        org_id: [...value].filter((v) => v != "all").join(","),
       });
     }
   };
 
+  const set2 = (
+    e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
+  ) => {
+    const fieldName = e.target.name;
+    const value = e.target.value;
+
+    setFormFields((oldValues) => ({ ...oldValues, [fieldName]: value }));
+  };
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setSendFormData(formFields);
+
+    // setSendFormData(data); actions
+  };
   return (
     <Stack>
-      <Form>
+      <form onSubmit={handleSubmit} data-testid="resource-add-form">
         <Box style={{ paddingTop: "15px" }}>
           <Grid
             style={{ alignSelf: "stretch", paddingBottom: "10px" }}
@@ -124,15 +83,14 @@ const FormAssessmentBox: React.FC<ViewProps> = ({
             xs={4}
           >
             <TextFieldComponent
-              style={{ backgroundColor: "white" }}
-              name="patient_firstname"
               required={true}
-              id="patientFirstname"
+              name="name"
+              id="name"
               label="Enter assessment name"
-              value={patientInputs}
-              onChange={(e) => handlePatientInputChange(e.target.value)}
+              value={formFields?.name}
+              onChange={set2}
               fullWidth={true}
-              inputProps={{ "data-testid": "patient_firstname" }}
+              inputProps={{ "data-testid": "assessment_name" }}
               variant="outlined"
               className="form-control-bg"
               size="small"
@@ -141,50 +99,25 @@ const FormAssessmentBox: React.FC<ViewProps> = ({
         </Box>
 
         <Box>
-          <FormControl style={{ width: "100%", backgroundColor: "" }}>
-            <InputLabel size="small" id="mutiple-select-label">
-              Select organisation*
-            </InputLabel>
-            <Select
-              data-testid="select_organisation1"
-              name="select_organisation"
-              label="Select_Organisation"
-              multiple
-              size="small"
-              value={selected}
-              renderValue={(selected) => {
-                /* istanbul ignore file */
-                const selectedOptions = organizationList.filter((option) =>
-                  selected.includes(option._id)
-                );
-                /* istanbul ignore next */ const selectedNames =
-                  selectedOptions.map((option) => option.name);
-                /* istanbul ignore next */ return selectedNames.join(", ");
-              }}
-              style={{
-                flex: "none",
-                order: "1",
-                flexGrow: "0",
-              }}
-            >
-              <CheckboxAll
-                isAllSelected={isAllSelected}
-                onChange={handleAllCheckboxChange}
-              />
-              {
-                /* istanbul ignore file */
-                organizationList.map((option) => (
-                  <OrganizationCheckbox
-                    data-testid={option._id}
-                    key={option._id}
-                    option={option}
-                    checked={selected.includes(option._id)}
-                    onChange={() => handleCheckboxChange(option._id)}
-                  />
-                ))
-              }
-            </Select>
-          </FormControl>
+          <MultiSelectComponent
+            fullWidth
+            required={true}
+            onChange={handleChange}
+            id="resourceOrgSelect"
+            labelId="resourceOrg"
+            name="org_id"
+            label="Select Organization"
+            options={[
+              ...[{ _id: "all", name: "Select All" }],
+              ...(organizationList || []),
+            ]}
+            mappingKeys={["_id", "name"]}
+            size="small"
+            className="form-control-bg multiSelect"
+            extraProps={{ "data-testid": "mainOrganizationSelect" }}
+            multiSelect={csvDecode(formFields?.org_id)}
+            value={csvDecode(formFields?.org_id)}
+          />
         </Box>
 
         <div
@@ -198,12 +131,12 @@ const FormAssessmentBox: React.FC<ViewProps> = ({
             style={{ paddingLeft: "30px", paddingRight: "30px" }}
             data-testid="addSubmitForm"
             variant="contained"
-            onClick={handleSubmit}
+            type="submit"
           >
             Save
           </Button>
         </div>
-      </Form>
+      </form>
     </Stack>
   );
 };
