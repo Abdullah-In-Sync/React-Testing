54520c90b13b9de5e85d47a523b04aa85df08469
ravindrabhanderi14 <117576691+ravindrabhanderi14@users.noreply.github.com>
2023-09-27 11:16:48 +0530

    Feat: reduce duplicates code

--- src/pages/therapist/formulation/view/[id]/index.tsx
+++ src/pages/therapist/formulation/view/[id]/index.tsx
@@ -1,56 +1,8 @@
-import { useLazyQuery } from "@apollo/client";
 import type { NextPage } from "next";
-import { useRouter } from "next/router";
-import { useEffect, useRef, useState } from "react";
-import ViewFormulation from "../../../../../components/admin/formulation/view/ViewFormulation";
-import ContentHeader from "../../../../../components/common/ContentHeader";
-import { ModalElement } from "../../../../../components/common/CustomModal/CommonModal";
-import Loader from "../../../../../components/common/Loader";
-import Layout from "../../../../../components/layout";
-import { GET_FORMULATION_BY_ID } from "../../../../../graphql/formulation/graphql";
-import { FormulationData } from "../../../../../graphql/formulation/types";
+import ViewFormulationPage from "../../../../admin/formulation/view/[id]";
 
-const ViewFormulationPage: NextPage = () => {
-  const router = useRouter();
-  const { query: { id: formulation_id } = {} } = router;
-  const [loader, setLoader] = useState<boolean>(true);
-  const infoModalRef = useRef<ModalElement>(null);
-
-  const [
-    getFormulation,
-    { data: { getFormulationById: formulationData = undefined } = {} },
-  ] = useLazyQuery<FormulationData>(GET_FORMULATION_BY_ID, {
-    fetchPolicy: "cache-and-network",
-    onCompleted: () => {
-      setLoader(false);
-    },
-  });
-
-  useEffect(() => {
-    getFormulation({
-      variables: { formulation_id },
-    });
-  }, []);
-
-  const backButtonClick = () => {
-    router.back();
-  };
-
-  return (
-    <>
-      <Layout>
-        <Loader visible={loader} />
-        <ContentHeader title="Formulation Detail" />
-        {formulationData && (
-          <ViewFormulation
-            data={formulationData}
-            infoModalRef={infoModalRef}
-            backButtonClick={backButtonClick}
-          />
-        )}
-      </Layout>
-    </>
-  );
+const ViewTherapistFormulationPage: NextPage = () => {
+  return <ViewFormulationPage />;
 };
 
-export default ViewFormulationPage;
+export default ViewTherapistFormulationPage;
