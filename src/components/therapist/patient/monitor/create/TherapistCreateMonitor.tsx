b8264d30d3f9352588df5ea1e486c49ccad07ba1
ravindrabhanderi14 <117576691+ravindrabhanderi14@users.noreply.github.com>
2023-10-03 14:44:35 +0530

    MYHF-2359

--- src/components/therapist/patient/monitor/create/TherapistCreateMonitor.tsx
+++ src/components/therapist/patient/monitor/create/TherapistCreateMonitor.tsx
@@ -1,11 +1,10 @@
 import { Formik } from "formik";
 import React from "react";
 import TherapistMonoitorCommonForm from "../form/TherapistMonitorCommonForm";
-import { ModalRefs } from "../../../../admin/monitor/form/types";
 import { TherapistMonitorValidationSchema } from "../form/therapistMonitorValidationSchema";
 import { defalutEmojis } from "../../../../../lib/constants";
 
-type ViewProps = any & ModalRefs;
+type ViewProps = any;
 
 const TherapistCreateMonitorForm: React.FC<ViewProps> = ({
   submitForm,
