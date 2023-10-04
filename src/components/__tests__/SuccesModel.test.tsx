e15660ec37bb163d81c87366e1585a8e1db3c77e
1711shub <114855116+1711shub@users.noreply.github.com>
2023-10-04 11:03:57 +0530

    Success modal test itself

--- src/components/__tests__/SuccesModel.test.tsx
+++ src/components/__tests__/SuccesModel.test.tsx
@@ -1,20 +1,27 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import { SuccessModal } from "../common/SuccessModal";
+import { SnackbarProvider } from "notistack";
 
 describe("when rendered with a `testid` prop", () => {
-  it("should display modal", () => {
+  it("should display modal", async () => {
     render(
-      <SuccessModal
-        isOpen={true}
-        title="Success"
-        description="test description"
-        onOk={() => null}
-      />
+      <SnackbarProvider>
+        <SuccessModal
+          isOpen={true}
+          title="Success"
+          description="test description"
+          onOk={() => null}
+        />
+      </SnackbarProvider>
     );
 
-    expect(screen.queryByTestId("description").textContent).toEqual(
-      "test description"
-    );
-    expect(screen.queryByTestId("title").textContent).toEqual("Success");
+    await waitFor(async () => {
+      expect(screen.queryByText("test description")).toBeInTheDocument();
+    });
+
+    // expect(screen.queryByTestId("description").textContent).toEqual(
+    //   "test description"
+    // );
+    // expect(screen.queryByTestId("title").textContent).toEqual("Success");
   });
 });
