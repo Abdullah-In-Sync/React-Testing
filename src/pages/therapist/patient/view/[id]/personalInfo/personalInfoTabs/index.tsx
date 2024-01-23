305987a91c30d96721265da830dedaedad4599b6
piyushmy <piyush.singh@myhelp.co.uk>
2024-01-23 14:31:18 +0530

    therapist agreement custom check

--- src/pages/therapist/patient/view/[id]/personalInfo/personalInfoTabs/index.tsx
+++ src/pages/therapist/patient/view/[id]/personalInfo/personalInfoTabs/index.tsx
@@ -4,8 +4,10 @@ import { useRouter } from "next/router";
 import TherapyTabsGenerator from "../../../../../../../components/common/TabsGenerator/TherapyTabGenerator";
 import TherapistProfileDetails from "../profileDetails";
 import TherapistProfileAgreement from "../profileAgreemet";
+import { checkPrivilageAccess } from "../../../../../../../utility/helper";
 
 export default function TherapyPersonalInfoTabs() {
+  const isCustomAgreement = checkPrivilageAccess("Agreement", "View");
   const router = useRouter();
   const {
     query: { id, mainTab },
@@ -17,12 +19,17 @@ export default function TherapyPersonalInfoTabs() {
       value: "details",
       component: <TherapistProfileDetails />,
     },
-    {
-      label: "Agreement",
-      value: "agreement",
-      component: <TherapistProfileAgreement />,
-    },
   ];
+
+  const isCustomAgreementTabs = isCustomAgreement
+    ? [
+        {
+          label: "Agreement",
+          value: "agreement",
+          component: <TherapistProfileAgreement />,
+        },
+      ]
+    : [];
 
   return (
     <Box
@@ -31,7 +38,7 @@ export default function TherapyPersonalInfoTabs() {
       className="therapyTabsWrapper"
     >
       <TherapyTabsGenerator
-        tabsList={tabs}
+        tabsList={[...tabs, ...isCustomAgreementTabs]}
         tabLabel={`/therapist/patient/view/${id}/?mainTab=${mainTab}&tab=`}
       />
     </Box>
